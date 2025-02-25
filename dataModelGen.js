416a18f0c94c1a7c71f6ed0aad8cd3cb39f2a581
cike <1028139084@qq.com>
2025-02-26 00:13:24 +0800

    fix:gen model

--- dataModelGen.js
+++ dataModelGen.js
@@ -9,9 +9,7 @@ function dataModelGen(swaggerData, modelDir) {
         var element = swaggerData.components.schemas[dtoName];
         var fileContentStringBuilder = "";
         var order = 1;
-
         if (element.hasOwnProperty("properties") || element.hasOwnProperty('allOf')) {
-
             if (element.hasOwnProperty('allOf') && element['allOf'].length >= 2) {
                 fileContentStringBuilder += `export type ${dtoName} = ${getType(element['allOf'][0])} & {\r\n`;
                 element = element['allOf'][1]
@@ -41,7 +39,9 @@ function dataModelGen(swaggerData, modelDir) {
             }
             fileContentStringBuilder += "}\r\n";
 
-        } else {
+        }else if(element.hasOwnProperty("type")&&element["type"]=="object"){
+            fileContentStringBuilder += `export type ${dtoName} = {\r\n}\r\n`;
+        }else {
             order = 0;
             fileContentStringBuilder += `export enum ${dtoName} {\r\n`;
             try {
